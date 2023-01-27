d289d12ccdd097a00cb22d7a8f915ac854699d60
Tiago Ribeiro <tiagobr15@gmail.com>
2023-01-26 23:38:41 -0300

    subindo organizacao pdf e atualizando o arquivo terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "barbero-devops-iac"
+  project = "play-comercial-mobile-dev"
   region  = "us-central1"
   zone    = "us-central1-c"
   credentials = "${file("serviceaccount.yaml")}"
@@ -10,35 +10,35 @@ resource "google_folder" "Financeiro" {
   parent       = "organizations/540829645030"
 }
 
-resource "google_folder" "SalesForce" {
-  display_name = "SalesForce"
-  parent       = google_folder.Financeiro.name
+resource "google_folder" "Comercial" {
+  display_name = "Comercial"
+  parent       = google_folder.Comercial.name
 }
 
 resource "google_folder" "Desenvolvimento" {
   display_name = "Desenvolvimento"
-  parent       = google_folder.SalesForce.name
+  parent       = google_folder.Comercial.name
 }
 
-resource "google_folder" "Producao" {
-  display_name = "Producao"
-  parent       = google_folder.SalesForce.name
+resource "google_folder" "Mobile" {
+  display_name = "Mobile"
+  parent       = google_folder.Comercial.name
 }
 
 
-resource "google_project" "barberosa2-salesforce-dev" {
-  name       = "SalesForce-Dev"
-  project_id = "barberosa2-salesforce-dev"
-  folder_id  = google_folder.Desenvolvimento.name
+resource "google_project" "play-erp--dev" {
+  name       = "play-erp--devv"
+  project_id = "play-erp--dev"
+  folder_id  = erp.name
   auto_create_network=false
   billing_account = "018973-A8340F-83D8E5"
 
 }
 
-# resource "google_project" "barberosa-salesforce-prod" {
-#   name       = "SalesForce-Prod"
-#   project_id = "barberosa-salesforce-prod"
-#   folder_id  = google_folder.Producao.name
+# resource "google_project" "play-erp-dev" {
+#   name       = "play-erp-dev"
+#   project_id = "play-erp-dev"
+#   folder_id  = google_folder.Desenvolvimento.name
 #   auto_create_network=false
 #   billing_account = "01B2C4-70459E-F53446"
 # }
